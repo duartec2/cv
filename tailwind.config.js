1183243d3ac2a6ab7e4de26273b2d128f3a8de85
Ricardo Duarte <duartec2@msn.com>
2023-06-06 21:44:55 -0600

    Fix rendering blockers

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     extend: {
       fontFamily: {
         'lato': ['Lato', ...defaultTheme.fontFamily.sans],
-        'quicksand': ['Quicksand', ...defaultTheme.fontFamily.sans],
+        //'quicksand': ['Quicksand', ...defaultTheme.fontFamily.sans],
         'syne': ['Syne', ...defaultTheme.fontFamily.sans],
       },
     }
